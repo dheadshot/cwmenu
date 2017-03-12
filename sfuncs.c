8fc87e85e8362e4f2ae52c547dc33968994b4308
Deadly Headshot <ddlyheadshot@gmail.com>
2017-03-12 17:22:19 +0000

    Started string functions module...

---
+++ sfuncs.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sfuncs.h"
+
+
+int streq_(char *a, char *b)
+{
+  unsigned long al, bl;
+  if (a == NULL || b == NULL) return 0;
+  al = strlen(a);
+  bl = strlen(b);
+  if (al != bl) return 0;
+  if (memcmp(a,b,al)==0) return 1;
+  return 0;
+}
+
+int startsame_(char *a, char *b)
+{
+  unsigned long al, bl;
+  if (a == NULL || b == NULL) return 0;
+  al = strlen(a);
+  bl = strlen(b);
+  if (al < bl)
+  {
+    if (memcmp(a,b,al)==0) return 1;
+  }
+  else
+  {
+    if (memcmp(a,b,bl)==0) return 1;
+  }
+  return 0;
+}
+
---
+++ sfuncs.h
@@ -0,0 +1,7 @@
+#ifndef __INC_SFUNCS_H__
+#define __INC_SFUNCS_H__
+
+int streq_(char *a, char *b);
+int startsame_(char *a, char *b);
+
+#endif
